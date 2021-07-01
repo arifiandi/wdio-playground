f4786d170170ef8cbb542cd1c388f8ff332bcc93
Joycelind <chowjoycelind@gmail.com>
2021-07-01 12:36:29 +0700

    add web automation

--- features/scenario/training/web-training.feature
+++ features/scenario/training/web-training.feature
@@ -3,3 +3,12 @@ Feature: Web Training Feature
 @training1
 Scenario: Go to certain website
     Given go to website
+
+@training2
+Scenario: Fill form
+    Given go to form page
+    When fill username
+    And fill enail
+    And set current address
+    And set permanent address
+    Then click submit button
--- features/step-definitions/training-steps/training-web-steps.js
+++ features/step-definitions/training-steps/training-web-steps.js
@@ -1,8 +1,43 @@
-const {Given} = require('@cucumber/cucumber')
+const { Given, Then, When } = require('@cucumber/cucumber')
 
 Given('go to website', function () {
-    // Write code here that turns the phrase above into concrete actions
-    // return 'pending';
-    browser.url('http://www.google.com')
-    driver.pause(5000)
-  });
+  // Write code here that turns the phrase above into concrete actions
+  // return 'pending';
+  browser.url('https://www.google.com')
+  driver.pause(5000)
+});
+
+Given('go to form page', function () {
+  // Write code here that turns the phrase above into concrete actions
+  browser.url('https://demoqa.com/text-box')
+});
+
+When('fill username', function () {
+  // Write code here that turns the phrase above into concrete actions
+  const fullName = $('#userName')
+  fullName.setValue('Joycelind')
+});
+
+When('fill enail', function () {
+  // Write code here that turns the phrase above into concrete actions
+  const email = $('#userEmail')
+  email.setValue('joy@test.com')
+});
+
+When('set current address', function () {
+  // Write code here that turns the phrase above into concrete actions
+  const currentAddress = $('#currentAddress')
+  currentAddress.setValue('Plaza Bank Index')
+});
+
+When('set permanent address', function () {
+  // Write code here that turns the phrase above into concrete actions
+  const permanentAddress = $('#permanentAddress')
+  permanentAddress.setValue('Pantai Indah Kapuk')
+});
+
+Then('click submit button', function () {
+  // Write code here that turns the phrase above into concrete actions
+  const submitButton = $('#submit')
+  submitButton.click()
+});

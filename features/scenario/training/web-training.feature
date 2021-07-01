5ba52f94f14b8c1349b3cec78c8612ee14b8977b
tomygunawan <tomygunawan24@gmail.com>
2021-07-01 12:35:35 +0700

    test web

--- features/scenario/training/web-training.feature
+++ features/scenario/training/web-training.feature
@@ -3,3 +3,12 @@ Feature: Web Training Feature
 @training1
 Scenario: Go to certain website
     Given go to website
+
+@training2
+Scenario: Fill simple form
+    Given go to webToolsQA
+    When fill fullName
+    And fill email
+    And fill currentAddress
+    And fill permanentAddress
+    Then click submit
--- features/step-definitions/training-steps/training-web-steps.js
+++ features/step-definitions/training-steps/training-web-steps.js
@@ -1,4 +1,8 @@
 const {Given} = require('@cucumber/cucumber')
+const {When} = require('@cucumber/cucumber')
+const {Then} = require('@cucumber/cucumber')
+
+
 
 Given('go to website', function () {
     // Write code here that turns the phrase above into concrete actions
@@ -6,3 +10,50 @@ Given('go to website', function () {
     browser.url('http://www.google.com')
     driver.pause(5000)
   });
+
+Given('go to webToolsQA', function () {
+    // Write code here that turns the phrase above into concrete actions
+    // return 'pending';
+    browser.url('https://demoqa.com/text-box')
+  });
+
+When('fill fullName', function () {
+      // Write code here that turns the phrase above into concrete actions
+      //return 'pending';
+    const fullName = $('#userName')
+    fullName.setValue('Tomy Gunawan')
+    //driver.pause(5000)
+    });
+
+When('fill email', function () {
+      // Write code here that turns the phrase above into concrete actions
+      //return 'pending';
+      const userEmail = $('#userEmail')
+      userEmail.setValue('Test@test.com')
+      //driver.pause(5000)
+    });
+
+When('fill currentAddress', function () {
+      // Write code here that turns the phrase above into concrete actions
+      //return 'pending';
+      const currentAddress = $('#currentAddress')
+      currentAddress.setValue('Alamat test no.2')
+      //driver.pause(5000)
+    });
+
+When('fill permanentAddress', function () {
+      // Write code here that turns the phrase above into concrete actions
+      //return 'pending';
+      const permanentAddress = $('#permanentAddress')
+      permanentAddress.setValue('Alamat permanent test no.1')
+      //driver.pause(5000)
+    });
+
+Then('click submit', function () {
+      // Write code here that turns the phrase above into concrete actions
+      //return 'pending';
+      const submitButton = $('#submit')
+      submitButton.click()
+      driver.pause(2000)
+    });
+

5f03a652c829783d64f81d7b269093a4ab13cb38
arifiandi <ridwanarifiandi@gmail.com>
2021-07-14 22:52:58 +0700

    Training 1

--- features/scenario/training/web-training.feature
+++ features/scenario/training/web-training.feature
@@ -2,4 +2,14 @@ Feature: Web Training Feature
 
 @training1
 Scenario: Go to certain website
-    Given go to website
+    Given go to "http://www.google.com"
+    When set status to "Not Connected"
+
+@training2
+Scenario: fill simple form
+    Given go to "https://demoqa.com/text-box"
+    When fill username
+    And fill email
+    And set current address
+    And set fixed address
+    Then click submit button
--- features/step-definitions/training-steps/training-web-steps.js
+++ features/step-definitions/training-steps/training-web-steps.js
@@ -1,8 +1,55 @@
-const {Given} = require('@cucumber/cucumber')
+const {Given, When, Then} = require('@cucumber/cucumber')
 
-Given('go to website', function () {
+// Given('go to website', function () {
+//     // Write code here that turns the phrase above into concrete actions
+//     // return 'pending';
+//     browser.url('http://www.google.com')
+//     driver.pause(5000)
+//   });
+
+  Given('go to {string}', function (web) {
     // Write code here that turns the phrase above into concrete actions
     // return 'pending';
-    browser.url('http://www.google.com')
+    browser.url(web)
+  });
+
+  // Given('go to form page', function () {
+  //   browser.url('https://demoqa.com/text-box')
+  // });
+
+  When('fill username', function () {
+    // Write code here that turns the phrase above into concrete actions
+    // return 'pending';
+    const fullName = $('#userName')
+    fullName.setValue('Ridwan Arifiandi')
     driver.pause(5000)
+
+  });
+
+  When('fill email', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  When('set current address', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  When('set fixed address', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  Then('click submit button', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  When('set status to {string}', function (string) {
+    // Write code here that turns the phrase above into concrete actions
+    if(string !== page.status.getText()){
+      link.click()
+    }
+    
   });
